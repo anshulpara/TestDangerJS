d3c7d7823e1e527e9aae59fac8637c1e6eb859b5
Anshul Parashar <anparashar@stubhub.com>
2019-08-26 20:22:00 +0530

    updated yarn

--- dangerfile.js
+++ dangerfile.js
@@ -2,10 +2,6 @@ const {danger, warn} = require('danger')
 
 // Fail if a new file is added to a root directory
 danger.git.created_files.forEach(filePath => {
-                                 console.log(filePath)
-                                 if filePath.includes('/') {
+                                 if (!filePath.includes('/')) {
                                  fail(`Failed because a new file is being added in the root directory: ${filePath}`);
-                                 }
-                                 })
-
-
+                                 }})
