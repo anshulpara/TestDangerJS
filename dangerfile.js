c3c2073a0ac9dae0fc719695a92ddeb645e8c3a8
Anshul Parashar <anparashar@stubhub.com>
2019-08-26 19:58:25 +0530

    updated yarn

--- dangerfile.js
+++ dangerfile.js
@@ -1,24 +1,14 @@
 const {danger, warn} = require('danger')
-const {danger, includes} = require('danger')
-
-// No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-    warn('Please include a description of your PR changes.');
-}
-
-// Keep Lockfile up to date
-const packageChanged = includes(danger.git.modified_files, 'package.json');
-const lockfileChanged = includes(danger.git.modified_files, 'yarn.lock');
-if (packageChanged && !lockfileChanged) {
-    const message = 'Changes were made to package.json, but not to yarn.lock';
-    const idea = 'Perhaps you need to run `yarn install`?';
-    warn(`${message} - <i>${idea}</i>`);
-}
 
 // Fail if a new file is added to a root directory
-var modifiedSpecFiles = danger.git.created_files.filter(function (filePath) {
-                                                         warn(`${filePath}`);
-                                                         return filePath.match(/-spec.(js|jsx|ts|tsx)$/gi);
-                                                         });
+danger.git.created_files.forEach(filePath => {
+                                 console.log(filePath)
+                                 console.log(filePath)
+
+//                                 if (filePath.includes('/')) {
+//                                 fail(`Failed because a new file is being added in the root directory: ${filePath}`);
+//                                 }
+
+                                 })
 
 
