02a70d0fed4c88b24548dfc85b3fbac33a9b3fed
Anshul Parashar <anparashar@stubhub.com>
2019-08-26 20:16:02 +0530

    updated yarn

---
+++ TestDangerJS/Vehicle1.swift
@@ -0,0 +1,9 @@
+//
+//  Vehicle1.swift
+//  TestDangerJS
+//
+//  Created by Parashar, Anshul(AWF) on 26/08/19.
+//  Copyright © 2019 Parashar, Anshul(AWF). All rights reserved.
+//
+
+import Foundation
--- dangerfile.js
+++ dangerfile.js
@@ -3,7 +3,7 @@ const {danger, warn} = require('danger')
 // Fail if a new file is added to a root directory
 danger.git.created_files.forEach(filePath => {
                                  console.log(filePath)
-                                 if (filePath.includes('/')) {
+                                 if filePath.includes('/') {
                                  fail(`Failed because a new file is being added in the root directory: ${filePath}`);
                                  }
                                  })
